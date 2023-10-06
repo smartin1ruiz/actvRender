327be82c9d3d26f71630fa82a955db82736d7c04
smartin1ruiz <smartin@itpfp.com>
2023-10-06 13:31:58 +0200

    tercer commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname, 'index.html')
+  res.sendFile(path.join(__dirname, 'index.html'))
 })
 
 app.listen(3000)
